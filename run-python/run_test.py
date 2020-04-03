c310a11bbae5921d24097962939f3b9b79b79749
Unknown <rgillespie@gmail.com>
2020-04-03 14:49:53 -0600

    typo

--- run-python/run_test.py
+++ run-python/run_test.py
@@ -5,7 +5,7 @@ def run_test(username,password):
     print(f"Username = {username}")
     print(f"Password = {password}")
 
-if __name__ == "__main__":l
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Please use this syntax:")
     parser.add_argument("-u", "--username", help="Example Username", type=str, required=True)
     parser.add_argument("-p", "--password", help="Example Password", type=str, required=True)
